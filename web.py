313de4db17ddf11e2dc0e1bb4fb15432b0eb86bf
Karlo <karlo.cesarec1@gmail.com>
2022-11-24 09:33:48 +0100

    7th try

--- web.py
+++ web.py
@@ -8,9 +8,10 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your productivity.")
 
 
 for index, todo in enumerate(todos):
@@ -21,6 +22,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-
-st.text_input(label="Enter a todo:", placeholder="Add a new todo...",
+st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
